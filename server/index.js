f8b15c50f164b2b31918c367bf89505627d6623d
Abhishek Bharti <bhartiabhishek310@gmail.com>
2025-01-15 00:56:02 +0530

    created first severse using express

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+// Purpose: To create a simple server using express
 const http = require("http");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-    console.log("New Request Received");
-    console.log(req.headers);
-    res.end("Hello from server");
+const app = express();
+app.get("/", (req, res) => {
+    res.end("Hello from Home Page");
 });
 
+app.get("/about", (req, res) => {
+    // res.end("Hello from About Page");
+    res.end("Hello from About Page "+"hey "+req.query.name+" you're age is "+req.query.age);
+});
+
+const myServer = http.createServer(app);
 myServer.listen(8000, ()=> console.log("Server Started"));
